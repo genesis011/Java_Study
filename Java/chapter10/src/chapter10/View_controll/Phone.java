cebaee895966326d1e89aeb5fccd7c90d2c4d341
kokoboa <universe.sns@gmail.com>
2019-09-26 16:58:14 +0900

    20190926-2

---
+++ Java/chapter10/src/chapter10/View_controll/Phone.java
@@ -0,0 +1,35 @@
+package chapter10.View_controll;
+
+import javafx.beans.property.SimpleStringProperty;
+
+public class Phone {
+	private SimpleStringProperty amartPhone;
+	private SimpleStringProperty image;
+	
+	public Phone(String smartPhone, String image) {
+		this.amartPhone = new SimpleStringProperty(smartPhone);
+		this.image =new SimpleStringProperty (image) ;
+	}
+
+	public String getAmartPhone() {
+		return amartPhone.get();
+	}
+
+	public void setAmartPhone(String amartPhone) {
+		this.amartPhone.set(amartPhone);
+//		this.amartPhone = new SimpleStringProperty(amartPhone);
+	}
+
+	public String getImage() {
+		return image.get();
+	}
+
+	public void setImage(String image) {
+		this.image.set(image);
+//		this.image =new SimpleStringProperty(image);
+	}
+	
+	
+	
+	
+}
